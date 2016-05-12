8a73facc227ccb1083e572201af71e265aa7b0ec
dajiangyou123 <fyly123@gmail.com>
2016-05-12 10:58:10 +0800

    finish 10.4

--- ch10/10_29.cc
+++ ch10/10_29.cc
@@ -1,4 +1,28 @@
+/*
+   实现使用流迭代器读取一个文本，存入一个vector中的string里。
+*/
+
+
 #include <iostream>
+#include <vector>
+#include <string>
 #include <fstream>
+#include <iterator>
 
 using namespace std;
+
+int main(int argc, char **argv)
+{
+	ifstream fileS(argv[1],ifstream::in);
+	istream_iterator<string> fileIn(fileS),eof;
+	vector<string> vecs;
+
+	while(fileIn != eof)
+		vecs.push_back(*fileIn++);
+
+	for(auto i: vecs)
+		cout<<i<<" ";
+	cout<<endl;
+	
+	return 0;
+}
